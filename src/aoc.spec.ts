339edf88f388f055932ec5f25411f70367177aac
David M. Lee <leedm777@yahoo.com>
2024-12-04 21:01:52 -0600

    Fixed tests for aoc.mod

--- src/aoc.spec.ts
+++ src/aoc.spec.ts
@@ -2,9 +2,13 @@ import { mod } from "./aoc";
 
 describe("aoc helpers", () => {
   describe("mod", () => {
-    it.each([[1, 2, 1]])("%s mod %s === %s", (lhs, rhs, expected) => {
+    it.each([
+      [1, 10, 1],
+      [-1, 10, 9],
+      [-10, 10, 0],
+    ])("%s mod %s === %s", (lhs, rhs, expected) => {
       const actual = mod(lhs, rhs);
-      expect(actual).toEqual(expected);
+      expect(actual).toStrictEqual(expected);
     });
   });
 });
