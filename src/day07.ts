290df1350d90badfa7c84de6d735a8d5374b70ac
David M. Lee <leedm777@yahoo.com>
2024-12-07 00:17:39 -0600

    Day 7, refactor
    Turn the operators into a parameter.

--- src/day07.ts
+++ src/day07.ts
@@ -15,7 +15,12 @@ function parseLine(input: string): Equation {
   return { expected: parseInt(expectedStr, 10), terms, acc };
 }
 
-function solveEquation({ expected, terms, acc }: Equation): boolean {
+type Operator = (lhs: number, rhs: number) => number;
+
+function solveEquation(
+  { expected, terms, acc }: Equation,
+  operators: Operator[] = [_.add, _.multiply],
+): boolean {
   if (terms.length === 0) {
     return acc === expected;
   }
@@ -26,26 +31,12 @@ function solveEquation({ expected, terms, acc }: Equation): boolean {
 
   const [firstTerm, ...restTerms] = terms;
 
-  const tryAdd = solveEquation({
-    expected,
-    terms: restTerms,
-    acc: acc + firstTerm,
-  });
-  if (tryAdd) {
-    return true;
-  }
-
-  const tryMultiply = solveEquation({
-    expected,
-    terms: restTerms,
-    acc: acc * firstTerm,
-  });
-
-  if (tryMultiply) {
-    return true;
-  }
-
-  return false;
+  return _.some(operators, (fn) =>
+    solveEquation(
+      { expected, terms: restTerms, acc: fn(acc, firstTerm) },
+      operators,
+    ),
+  );
 }
 
 export function part1(input: string[]) {
@@ -61,54 +52,11 @@ function numcat(lhs: number, rhs: number) {
   return parseInt(`${lhs}${rhs}`, 10);
 }
 
-function solveEquationWithCat({ expected, terms, acc }: Equation): boolean {
-  if (terms.length === 0) {
-    return acc === expected;
-  }
-
-  if (acc > expected) {
-    return false;
-  }
-
-  const [firstTerm, ...restTerms] = terms;
-
-  const tryAdd = solveEquationWithCat({
-    expected,
-    terms: restTerms,
-    acc: acc + firstTerm,
-  });
-  if (tryAdd) {
-    return true;
-  }
-
-  const tryMultiply = solveEquationWithCat({
-    expected,
-    terms: restTerms,
-    acc: acc * firstTerm,
-  });
-
-  if (tryMultiply) {
-    return true;
-  }
-
-  const tryCat = solveEquationWithCat({
-    expected,
-    terms: restTerms,
-    acc: numcat(acc, firstTerm),
-  });
-
-  if (tryCat) {
-    return true;
-  }
-
-  return false;
-}
-
 export function part2(input: string[]) {
   const equations = _.map(input, parseLine);
 
   return _(equations)
-    .filter((eq) => solveEquationWithCat(eq))
+    .filter((eq) => solveEquation(eq, [_.add, _.multiply, numcat]))
     .map("expected")
     .sum();
 }
