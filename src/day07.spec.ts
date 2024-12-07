8013c12abaa16866df6100b9b17e30e6e7c8274b
David M. Lee <leedm777@yahoo.com>
2024-12-06 23:49:54 -0600

    Day 7, part 1
    Works on sample, but not the puzzle input :-(

---
+++ src/day07.spec.ts
@@ -0,0 +1,41 @@
+import { part1, part2 } from "./day07";
+import { readInput } from "./aoc";
+
+const puzzleInput = readInput("./src/day07.txt");
+const exampleInput = [
+  "190: 10 19",
+  "3267: 81 40 27",
+  "83: 17 5",
+  "156: 15 6",
+  "7290: 6 8 6 15",
+  "161011: 16 10 13",
+  "192: 17 8 14",
+  "21037: 9 7 18 13",
+  "292: 11 6 16 20",
+];
+
+describe("day07", () => {
+  describe("part 1", () => {
+    it("should work with the sample", () => {
+      const actual = part1(exampleInput);
+      expect(actual).toStrictEqual(3749);
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part1(puzzleInput);
+      expect(actual).not.toStrictEqual(5539206647201);
+      expect(actual).toStrictEqual("TODO");
+    });
+  });
+
+  // eslint-disable-next-line jest/no-disabled-tests
+  describe.skip("part 2", () => {
+    it("should work with the sample", () => {
+      const actual = part2(exampleInput);
+      expect(actual).toStrictEqual("TODO");
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part2(puzzleInput);
+      expect(actual).toStrictEqual("TODO");
+    });
+  });
+});
---
+++ src/day07.ts
@@ -0,0 +1,62 @@
+import _ from "lodash";
+
+type Equation = {
+  expected: number;
+  terms: number[];
+};
+
+function parseLine(input: string): Equation {
+  const [expectedStr, termsStr] = input.split(": ");
+  const terms = _(termsStr)
+    .split(" ")
+    .map((s) => parseInt(s, 10))
+    .value();
+  return { expected: parseInt(expectedStr, 10), terms };
+}
+
+function solveEquation(
+  { expected, terms }: Equation,
+  acc: number = 0,
+): boolean {
+  if (terms.length === 0) {
+    return acc === expected;
+  }
+
+  if (acc >= expected) {
+    return false;
+  }
+
+  const [firstTerm, ...restTerms] = terms;
+
+  const tryAdd = solveEquation({ expected, terms: restTerms }, acc + firstTerm);
+  if (tryAdd) {
+    return true;
+  }
+
+  if (acc !== 0) {
+    const tryMultiply = solveEquation(
+      { expected, terms: restTerms },
+      acc * firstTerm,
+    );
+
+    if (tryMultiply) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+export function part1(input: string[]) {
+  const equations = _.map(input, parseLine);
+
+  return _(equations)
+    .filter((eq) => solveEquation(eq))
+    .map("expected")
+    .sum();
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function part2(input: string[]) {
+  return "TODO";
+}
