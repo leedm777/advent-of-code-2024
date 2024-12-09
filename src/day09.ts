68d073bee03007cfc45a84ffafa9dff873145ab9
David M. Lee <leedm777@yahoo.com>
2024-12-09 14:03:16 -0600

    Day 9, part 1, optimize
    Use what I did in part 2 to make part 1 less annoying when I run it.

--- src/day09.ts
+++ src/day09.ts
@@ -1,51 +1,12 @@
 import _ from "lodash";
 
-export function part1([input]: string[]) {
-  const { blocks } = _(input)
-    .map((ch) => parseInt(ch))
-    .reduce(
-      ({ nextId, blocks }, numBlocks, idx) => {
-        if (numBlocks === 0) {
-          return { nextId, blocks };
-        }
+type BlockRange = {
+  id: number;
+  offset: number;
+  numBlocks: number;
+};
 
-        if ((idx & 1) === 0) {
-          // file type
-          const id = nextId++;
-          blocks = _.concat(blocks, _.times(numBlocks, _.constant(id)));
-          return { nextId, blocks };
-        }
-
-        // free type
-        blocks = _.concat(blocks, _.times(numBlocks, _.constant(-1)));
-        return { nextId, blocks };
-      },
-      {
-        nextId: 0,
-        blocks: [] as number[],
-      },
-    );
-
-  for (let i = 0; i < blocks.length; ++i) {
-    if (blocks[i] === -1) {
-      let v = blocks.pop();
-      while (v === -1) {
-        v = blocks.pop();
-      }
-      blocks[i] = v || NaN;
-    }
-  }
-
-  return _(blocks)
-    .map((id, idx) => id * idx)
-    .sum();
-}
-
-function sumOfSeries(n: number, first: number, last: number = first + n - 1) {
-  return (n * (first + last)) / 2;
-}
-
-export function part2([input]: string[]) {
+function parseInput(input: string): BlockRange[] {
   let id = 0;
   let offset = 0;
   const blocks = _(input)
@@ -61,6 +22,61 @@ export function part2([input]: string[]) {
     })
     .reject(({ numBlocks }) => numBlocks === 0)
     .value();
+  return blocks;
+}
+
+function sumOfSeries(n: number, first: number, last: number = first + n - 1) {
+  return (n * (first + last)) / 2;
+}
+
+function checksum(blocks: BlockRange[]) {
+  return _(blocks)
+    .filter(({ id }) => id !== -1)
+    .map(
+      // Checksum is id * offset + id * (offset + 1) + id * (offset + 2) ...
+      // We can change that to id * sumOfSeries(offset, numBlocks)
+      ({ id, numBlocks, offset }) => id * sumOfSeries(numBlocks, offset),
+    )
+    .sum();
+}
+
+export function part1([input]: string[]) {
+  const blocks = parseInput(input);
+
+  for (let i = blocks.length - 1; i >= 0; --i) {
+    if (blocks[i].id === -1) {
+      // skip free blocks
+      continue;
+    }
+
+    const fileBlock = blocks[i];
+    for (let j = 0; j < i; ++j) {
+      if (blocks[j].id !== -1 || blocks[j].numBlocks <= 0) {
+        // skip file or empty free blocks
+        continue;
+      }
+      const freeBlock = blocks[j];
+
+      if (freeBlock.numBlocks >= fileBlock.numBlocks) {
+        // Move the file into the free block, some will be left over
+        fileBlock.offset = freeBlock.offset;
+        // allocate the free space
+        freeBlock.numBlocks -= fileBlock.numBlocks;
+        freeBlock.offset += fileBlock.numBlocks;
+        break;
+      } else {
+        // Move part of the file into the free block
+        freeBlock.id = fileBlock.id;
+        fileBlock.numBlocks -= freeBlock.numBlocks;
+      }
+    }
+  }
+
+  return checksum(blocks);
+}
+
+export function part2([input]: string[]) {
+  const blocks = parseInput(input);
 
   for (let i = blocks.length - 1; i >= 0; --i) {
     if (blocks[i].id === -1) {
@@ -87,12 +103,5 @@ export function part2([input]: string[]) {
     }
   }
 
-  return _(blocks)
-    .filter(({ id }) => id !== -1)
-    .map(
-      // Checksum is id * offset + id * (offset + 1) + id * (offset + 2) ...
-      // We can change that to id * sumOfSeries(offset, numBlocks)
-      ({ id, numBlocks, offset }) => id * sumOfSeries(numBlocks, offset),
-    )
-    .sum();
+  return checksum(blocks);
 }
