005b71eefdfe8bf578349584eab01cf0ae303d94
David M. Lee <leedm777@yahoo.com>
2024-12-11 08:08:11 -0600

    Day 11, part 2, WIP
    Less ugly optimization. Part 2 no longer runs out of memory, but still does not complete.

--- src/day11.ts
+++ src/day11.ts
@@ -1,5 +1,6 @@
 import _ from "lodash";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function blink(stones: number[]): number[] {
   return _.reduce(
     stones,
@@ -26,15 +27,34 @@ function blink(stones: number[]): number[] {
   );
 }
 
+function blinkLen(stone: number, numBlinks: number): number {
+  if (numBlinks === 0) {
+    return 1;
+  }
+
+  if (stone === 0) {
+    return blinkLen(1, numBlinks - 1);
+  }
+
+  const numDigits = Math.floor(Math.log10(stone)) + 1;
+  if ((numDigits & 1) === 0) {
+    const factor = 10 ** (numDigits >> 1);
+    const leftStone = Math.floor(stone / factor);
+    const rightStone = stone - leftStone * factor;
+    return (
+      blinkLen(leftStone, numBlinks - 1) + blinkLen(rightStone, numBlinks - 1)
+    );
+  }
+
+  return blinkLen(2024 * stone, numBlinks - 1);
+}
+
 export function part1([input]: string[], numIterations = 25) {
-  let stones = _(input)
+  return _(input)
     .split(" ")
     .map((s) => parseInt(s, 10))
-    .value();
-  for (let i = 0; i < numIterations; ++i) {
-    stones = blink(stones);
-  }
-  return stones.length;
+    .map((stone) => blinkLen(stone, numIterations))
+    .sum();
 }
 
 export function part2(input: string[]) {
