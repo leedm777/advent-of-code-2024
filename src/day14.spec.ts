803db26fa840b5267ce9273e0fe1a2d238709e8a
David M. Lee <leedm777@yahoo.com>
2024-12-13 23:26:40 -0600

    Day 14, part 1

---
+++ src/day14.spec.ts
@@ -0,0 +1,43 @@
+import { part1, part2 } from "./day14.ts";
+import { readInput } from "./aoc.ts";
+
+const puzzleInput = readInput("./inputs/day14.txt");
+const exampleInput = [
+  "p=0,4 v=3,-3",
+  "p=6,3 v=-1,-3",
+  "p=10,3 v=-1,2",
+  "p=2,0 v=2,-1",
+  "p=0,0 v=1,3",
+  "p=3,0 v=-2,-2",
+  "p=7,6 v=-1,-3",
+  "p=3,0 v=-1,-2",
+  "p=9,3 v=2,3",
+  "p=7,3 v=-1,2",
+  "p=2,4 v=2,-3",
+  "p=9,5 v=-3,-3",
+];
+
+describe("day14", () => {
+  describe("part 1", () => {
+    it("should work with the sample", () => {
+      const actual = part1(exampleInput, [11, 7]);
+      expect(actual).toStrictEqual(12);
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part1(puzzleInput);
+      expect(actual).toStrictEqual(229839456);
+    });
+  });
+
+  // eslint-disable-next-line jest/no-disabled-tests
+  describe.skip("part 2", () => {
+    it("should work with the sample", () => {
+      const actual = part2(exampleInput);
+      expect(actual).toStrictEqual("TODO");
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part2(puzzleInput);
+      expect(actual).toStrictEqual("TODO");
+    });
+  });
+});
---
+++ src/day14.ts
@@ -0,0 +1,81 @@
+import _ from "lodash";
+import { mod } from "./aoc.ts";
+
+type Robot = {
+  pos: [number, number];
+  vel: [number, number];
+};
+
+function p(str: string): number {
+  return parseInt(str, 10);
+}
+
+function parseRobot(str: string): Robot {
+  const [, px, py, vx, vy] = str.match(/^p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)/)!;
+  return {
+    pos: [p(px), p(py)],
+    vel: [p(vx), p(vy)],
+  };
+}
+
+function moveRobot(
+  robot: Robot,
+  numSeconds: number,
+  size: [number, number],
+): Robot {
+  return {
+    vel: robot.vel,
+    pos: [
+      mod(robot.pos[0] + robot.vel[0] * numSeconds, size[0]),
+      mod(robot.pos[1] + robot.vel[1] * numSeconds, size[1]),
+    ],
+  };
+}
+
+function findQuadant(pos: [number, number], size: [number, number]) {
+  const mid = _.map(size, (n) => (n - 1) / 2);
+
+  if (pos[0] === mid[0] || pos[1] === mid[1]) {
+    return "middle";
+  }
+
+  if (pos[0] < mid[0] && pos[1] < mid[1]) {
+    return "Q1";
+  }
+
+  if (pos[0] > mid[0] && pos[1] < mid[1]) {
+    return "Q2";
+  }
+
+  if (pos[0] > mid[0] && pos[1] > mid[1]) {
+    return "Q3";
+  }
+
+  if (pos[0] < mid[0] && pos[1] > mid[1]) {
+    return "Q4";
+  }
+
+  throw new Error(
+    "Math does not work that way, but convince TypeScript of that...",
+  );
+}
+
+export function part1(input: string[], size: [number, number] = [101, 103]) {
+  const robots = _.map(input, parseRobot);
+
+  return _(robots)
+    .map((r) => moveRobot(r, 100, size))
+    .groupBy((r) => findQuadant(r.pos, size))
+    .map((bots, quad) => ({
+      numBots: bots.length,
+      quad,
+    }))
+    .filter(({ quad }) => quad !== "middle")
+    .map("numBots")
+    .reduce(_.multiply);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function part2(input: string[]) {
+  return "TODO";
+}
