9f6a96bb16db2d2c55439ab45d251be2344dbc36
David M. Lee <leedm777@yahoo.com>
2024-12-13 22:20:47 -0600

    Day 12, part 1, refactor
    Use a different data structure to maybe make part 2 easier.

--- src/day12.ts
+++ src/day12.ts
@@ -1,80 +1,93 @@
 import _ from "lodash";
 import { move } from "./aoc.ts";
 
-type Stats = {
-  id: string;
+type Dir = [number, number];
+const neighbors: [Dir, Dir, Dir, Dir] = [
+  [0, 1], // right
+  [1, 0], // down
+  [0, -1], // left
+  [-1, 0], // up
+];
+
+type Plot = {
+  plant: string;
+  region: number;
+  hasBorder: [boolean, boolean, boolean, boolean];
+};
+
+function floodFill(
+  map: Plot[][],
+  startPos: [number, number],
+  regionId: number,
+) {
+  // from https://en.wikipedia.org/wiki/Flood_fill
+  const q: number[][] = [startPos];
+  const { plant } = _.get(map, startPos);
+  while (!_.isEmpty(q)) {
+    const pos = q.pop()!;
+    const plot = _.get(map, pos);
+    if (!plot || !_.isNaN(plot.region) || plot.plant !== plant) {
+      continue;
+    }
+
+    plot.region = regionId;
+    q.push(..._.map(neighbors, (dir) => move(pos, dir)));
+  }
+}
+
+type Region = {
+  id: number;
+  plant: string;
   area: number;
   perimeter: number;
 };
 
-const neighbors = [
-  [0, 1],
-  [1, 0],
-  [0, -1],
-  [-1, 0],
-];
-
-function fillRegion(
-  map: string[],
-  regions: number[][],
-  pos: number[],
-  ch: string,
-  regionId: number,
-) {
-  const todo = [pos];
-
-  while (todo.length > 0) {
-    const next = todo.pop()!;
-    if (!_.get(regions, next)) {
-      _.set(regions, next, regionId);
-      // eslint-disable-next-line lodash/prefer-map
-      _(neighbors)
-        .map((dir) => move(next, dir))
-        .filter((nPos) => _.get(map, nPos) === ch)
-        .forEach((nPos) => {
-          todo.push(nPos);
-        });
-    }
-  }
-}
-
-export function part1(input: string[]) {
-  const stats: { [key: number]: Stats } = {};
-  const regions: number[][] = [];
-  let nextRegionId = 1;
-
-  _.forEach(input, (row, rowNum) => {
-    _.forEach(row, (ch, colNum) => {
+function parseInput(input: string[]) {
+  const map = _.map(input, (line, rowNum) =>
+    _.map(line, (plant, colNum) => {
       const pos = [rowNum, colNum];
-      const regionId = _.get(regions, pos);
+      const hasBorder = _.map(neighbors, (dir) => {
+        const neighbotPlant = _.get(input, move(pos, dir), ".");
+        return neighbotPlant !== plant;
+      });
+      return { plant, region: NaN, hasBorder } as Plot;
+    }),
+  );
 
-      if (!regionId) {
-        fillRegion(input, regions, pos, ch, nextRegionId++);
+  // setup regions
+  let regionId = 0;
+  _.forEach(map, (line, rowNum) => {
+    _.forEach(line, (plot, colNum) => {
+      if (_.isNaN(plot.region)) {
+        floodFill(map, [rowNum, colNum], regionId++);
       }
     });
   });
+  return map;
+}
 
-  _.forEach(input, (row, rowNum) => {
-    _.forEach(row, (ch, colNum) => {
-      const pos = [rowNum, colNum];
-      const regionId = _.get(regions, pos);
-      stats[regionId] ||= {
-        id: ch,
-        area: 0,
-        perimeter: 0,
-      };
+export function part1(input: string[]) {
+  const map = parseInput(input);
 
-      ++stats[regionId].area;
-      stats[regionId].perimeter += _(neighbors)
-        .map((dir) => move(pos, dir))
-        .filter((nPos) => ch !== _.get(input, nPos, "."))
-        .size();
-    });
-  });
+  return _.chain(map)
+    .flatten()
+    .reduce((regions, plot) => {
+      if (!regions[plot.region]) {
+        regions[plot.region] = {
+          id: plot.region,
+          plant: plot.plant,
+          area: 0,
+          perimeter: 0,
+        };
+      }
 
-  return _(stats)
+      ++regions[plot.region].area;
+      regions[plot.region].perimeter += _.sum(plot.hasBorder);
+      return regions;
+    }, [] as Region[])
     .map(({ area, perimeter }) => area * perimeter)
-    .sum();
+    .sum()
+    .value();
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
