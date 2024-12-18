97850e4670ea8c9806bb328ad591a822cde0a80c
David M. Lee <leedm777@yahoo.com>
2024-12-17 20:13:34 -0600

    Day 17, part 1
    I really enjoy the emulator puzzles :-)

---
+++ src/day17.spec.ts
@@ -0,0 +1,70 @@
+import { part1, part2, runComputer, Computer } from "./day17.ts";
+import { readInput } from "./aoc.ts";
+
+const puzzleInput = readInput("./inputs/day17.txt");
+const exampleInput = [
+  "Register A: 729",
+  "Register B: 0",
+  "Register C: 0",
+  "",
+  "Program: 0,1,5,4,3,0",
+];
+
+const otherExamples: [Partial<Computer>, Partial<Computer>][] = [
+  [
+    {
+      c: 9,
+      instructionPointer: 0,
+      program: [2, 6],
+    },
+    { b: 1 },
+  ],
+  [{ a: 10, program: [5, 0, 5, 1, 5, 4] }, { output: [0, 1, 2] }],
+  [
+    { a: 2024, program: [0, 1, 5, 4, 3, 0] },
+    { output: [4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0], a: 0 },
+  ],
+  [{ b: 29, program: [1, 7] }, { b: 26 }],
+  [{ b: 2024, c: 43690, program: [4, 0] }, { b: 44354 }],
+];
+
+const EmptyComputer = {
+  a: 0,
+  b: 0,
+  c: 0,
+  program: [],
+  instructionPointer: 0,
+  output: [],
+};
+
+describe("day17", () => {
+  describe("part 1", () => {
+    it.each(otherExamples)("should work for %s", (computer, expected) => {
+      const actual = runComputer({
+        ...EmptyComputer,
+        ...computer,
+      });
+      expect(actual).toStrictEqual(expect.objectContaining(expected));
+    });
+    it("should work with the sample", () => {
+      const actual = part1(exampleInput);
+      expect(actual).toStrictEqual("4,6,3,5,6,3,5,2,1,0");
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part1(puzzleInput);
+      expect(actual).toStrictEqual("2,7,6,5,6,0,2,3,1");
+    });
+  });
+
+  // eslint-disable-next-line jest/no-disabled-tests
+  describe.skip("part 2", () => {
+    it("should work with the sample", () => {
+      const actual = part2(exampleInput);
+      expect(actual).toStrictEqual("TODO");
+    });
+    it("should work with the puzzle input", () => {
+      const actual = part2(puzzleInput);
+      expect(actual).toStrictEqual("TODO");
+    });
+  });
+});
---
+++ src/day17.ts
@@ -0,0 +1,155 @@
+import _ from "lodash";
+
+function parseRegister(regStr: string) {
+  return _(regStr)
+    .split(": ")
+    .thru(([, s]) => parseInt(s, 10))
+    .value();
+}
+
+export type Computer = {
+  a: number;
+  b: number;
+  c: number;
+  program: number[];
+  instructionPointer: number;
+  output: number[];
+};
+
+function parseComputer(input: string[]): Computer {
+  const [aStr, bStr, cStr, , programStr] = input;
+  const a = parseRegister(aStr);
+  const b = parseRegister(bStr);
+  const c = parseRegister(cStr);
+  const program = _(programStr)
+    .split(": ")
+    .thru(([, s]) =>
+      _(s)
+        .split(",")
+        .map((s) => parseInt(s, 10))
+        .value(),
+    )
+    .value();
+
+  return {
+    a,
+    b,
+    c,
+    program,
+    instructionPointer: 0,
+    output: [],
+  };
+}
+
+function isHalted(c: Computer): boolean {
+  return c.instructionPointer >= c.program.length;
+}
+
+export function tick(c: Computer): Computer {
+  let instructionPointer = c.instructionPointer;
+  const opcode = c.program[instructionPointer++];
+  const operand = c.program[instructionPointer++];
+  const literal = operand;
+
+  let combo = NaN;
+  switch (operand) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+      combo = operand;
+      break;
+    case 4:
+      combo = c.a;
+      break;
+    case 5:
+      combo = c.b;
+      break;
+    case 6:
+      combo = c.c;
+      break;
+    case 7:
+      combo = NaN;
+      break;
+    default:
+      throw new Error(`Invalid operand ${operand}`);
+  }
+
+  switch (opcode) {
+    case 0:
+      // adv
+      return {
+        ...c,
+        instructionPointer,
+        a: Math.floor(c.a / (1 << combo)),
+      };
+    case 1:
+      // bxl
+      return {
+        ...c,
+        instructionPointer,
+        b: c.b ^ literal,
+      };
+    case 2:
+      // bst
+      return {
+        ...c,
+        instructionPointer,
+        b: combo % 8,
+      };
+    case 3:
+      // jnz
+      return {
+        ...c,
+        instructionPointer: c.a === 0 ? instructionPointer : literal,
+      };
+    case 4:
+      // bxc
+      return {
+        ...c,
+        instructionPointer,
+        b: c.b ^ c.c,
+      };
+    case 5:
+      // out
+      return {
+        ...c,
+        instructionPointer,
+        output: [...c.output, combo % 8],
+      };
+    case 6:
+      // bdv
+      return {
+        ...c,
+        instructionPointer,
+        b: Math.floor(c.a / (1 << combo)),
+      };
+    case 7:
+      return {
+        ...c,
+        instructionPointer,
+        c: Math.floor(c.a / (1 << combo)),
+      };
+    default:
+      throw new Error(`Invalid opcode ${opcode}`);
+  }
+}
+
+export function runComputer(c: Computer) {
+  while (!isHalted(c)) {
+    c = tick(c);
+  }
+  return c;
+}
+
+export function part1(input: string[]) {
+  let c = parseComputer(input);
+  c = runComputer(c);
+
+  return _.join(c.output);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function part2(input: string[]) {
+  return "TODO";
+}
