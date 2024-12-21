57b235cbb45bcecf8fc3103bdbb0588bdebe769b
David M. Lee <leedm777@yahoo.com>
2024-12-21 08:32:09 -0600

    Day 21, part 1, WIP
    It's closer, but still one path it's not solving right.

--- src/day21.spec.ts
+++ src/day21.spec.ts
@@ -1,9 +1,23 @@
-import { NumericNeighbors, part1, part2 } from "./day21.ts";
+import {
+  NumericNeighbors,
+  DirNeighbors,
+  part1,
+  part2,
+  punchIt,
+  moveKeypad,
+} from "./day21.ts";
 import { readInput } from "./aoc.ts";
 import _ from "lodash";
 
 const puzzleInput = readInput("./inputs/day21.txt");
 const exampleInput = ["029A", "980A", "179A", "456A", "379A"];
+const exampleExpecteds = [
+  "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A",
+  "<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A",
+  "<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A",
+  "<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A",
+  "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A",
+];
 
 describe("day21", () => {
   describe("part 1", () => {
@@ -11,13 +25,44 @@ describe("day21", () => {
       it("should be consistent", () => {
         _.forEach(NumericNeighbors, (toArray, from) => {
           for (const to of toArray) {
-            expect(NumericNeighbors[to]).toStrictEqual(
-              expect.arrayContaining([from]),
+            expect(NumericNeighbors[to.substring(0, 1)]).toStrictEqual(
+              expect.arrayContaining([
+                expect.stringMatching(new RegExp(`^${from}[<v>^]$`)),
+              ]),
             );
           }
         });
       });
     });
+    describe("DirNeighbors", () => {
+      it("should be consistent", () => {
+        _.forEach(DirNeighbors, (toArray, from) => {
+          for (const to of toArray) {
+            expect(DirNeighbors[to.substring(0, 1)]).toStrictEqual(
+              expect.arrayContaining([
+                expect.stringMatching(new RegExp(`^[${from}][<v>^]$`)),
+              ]),
+            );
+          }
+        });
+      });
+    });
+    describe("moveKeypad", () => {
+      it.each([["A", "4", "^^<<", NumericNeighbors]])(
+        "should for %s -> %s === %s",
+        (from, to, expected, keypad) => {
+          const actual = moveKeypad(from, to, keypad);
+          expect(actual).toStrictEqual(expected);
+        },
+      );
+    });
+    it.each(_.zip(exampleInput, exampleExpecteds))(
+      "should work for %s",
+      (input, expected) => {
+        const actual = punchIt(input!);
+        expect(actual.length).toStrictEqual(expected?.length);
+      },
+    );
     it("should work with the sample", () => {
       const actual = part1(exampleInput);
       expect(actual).toStrictEqual(126384);
--- src/day21.ts
+++ src/day21.ts
@@ -1,11 +1,21 @@
 import _ from "lodash";
 import { dijkstraHeuristic, findPath, Graph } from "./aoc.ts";
 
-function keyDirectional(sequence: string): string {
-  return sequence;
-}
+type Keypad = { [from: string]: string[] };
+export const DirNeighbors: Keypad = {
+  //     +---+---+
+  //     | ^ | A |
+  // +---+---+---+
+  // | < | v | > |
+  // +---+---+---+
+  "<": ["v>"],
+  v: ["<<", ">>", "^^"],
+  ">": ["v<", "A^"],
+  "^": ["vv", "A>"],
+  A: [">v", "^<"],
+};
 
-export const NumericNeighbors: { [from: string]: string[] } = {
+export const NumericNeighbors: Keypad = {
   // +---+---+---+
   // | 7 | 8 | 9 |
   // +---+---+---+
@@ -18,21 +28,30 @@ export const NumericNeighbors: { [from: string]: string[] } = {
   "0": ["2^", "A>"],
   "1": ["4^", "2>"],
   "2": ["1<", "5^", "3>", "0v"],
-  "3": ["2<", "6^", "Av"],
+  "3": ["6^", "2<", "Av"],
   "4": ["7^", "5>", "1v"],
   "5": ["4<", "8^", "6>", "2v"],
   "6": ["5<", "9^", "3v"],
   "7": ["8>", "4v"],
   "8": ["7<", "5v", "9>"],
   "9": ["8<", "6v"],
-  A: ["0<", "3^"],
+  A: ["3^", "0<"],
 };
 
-function moveNumeric(a: string, b: string): string {
+export function moveKeypad(a: string, b: string, keypad: Keypad): string {
   const g: Graph<string> = {
-    getNeighborDistance: _.constant(1),
+    getNeighborDistance(node1: string, node2: string): number {
+      const dir1 = node1.substring(1);
+      const dir2 = node2.substring(1);
+      // prefer consecutive sequences in the same direction; provides shorter
+      // paths for the robot controlling this robot
+      if (dir1 === dir2) {
+        return 1;
+      }
+      return 10;
+    },
     getNeighbors(node: string): string[] {
-      return NumericNeighbors[node.substring(0, 1)];
+      return keypad[node.substring(0, 1)];
     },
     h: dijkstraHeuristic,
     isGoal(node: string): boolean {
@@ -46,27 +65,41 @@ function moveNumeric(a: string, b: string): string {
   return _(g).thru(findPath).drop(1).invokeMap("substring", 1).join("");
 }
 
-function keyNumeric(sequence: string): string {
+export function movesForSequence(sequence: string, keypad: Keypad) {
   let prior = "A";
   let nextSequence = "";
   for (const key of sequence) {
-    nextSequence += moveNumeric(prior, key);
+    nextSequence += moveKeypad(prior, key, keypad);
     nextSequence += "A";
     prior = key;
   }
   return nextSequence;
 }
 
+function keyDirectional(sequence: string): string {
+  return movesForSequence(sequence, DirNeighbors);
+}
+
+function keyNumeric(sequence: string): string {
+  return movesForSequence(sequence, NumericNeighbors);
+}
+
+export function punchIt(numericSequence: string): string {
+  const dir1 = keyNumeric(numericSequence);
+  const dir2 = keyDirectional(dir1);
+  const dir3 = keyDirectional(dir2);
+
+  console.log([dir3, dir2, dir1, numericSequence].join("\n"));
+
+  return dir3;
+}
+
 export function part1(input: string[]) {
   return _(input)
     .map((numericSequence) => {
-      const dir1 = keyNumeric(numericSequence);
-      const dir2 = keyDirectional(dir1);
-      const dir3 = keyDirectional(dir2);
-      const dir4 = keyDirectional(dir3);
-
+      const seq = punchIt(numericSequence);
       const val = parseInt(numericSequence, 10);
-      return val * dir4.length;
+      return val * seq.length;
     })
     .sum();
 }
